ed196f7463fb2f96128466eac13f98682edd52f9
Rohit <rohitsansapkal@gmail.com>
2020-03-28 23:57:44 +0530

    firstcommit

---
+++ cbvproject1/testapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import View,TemplateView
+from django.http import HttpResponse
+# Create your views here.
+class HelloWorldView(View):
+    def get(self,request):
+        return HttpResponse('<h1>This is from class based view</h1>')
+class HelloWorldTemplateView(TemplateView):
+
+    template_name='testapp/result.html'
